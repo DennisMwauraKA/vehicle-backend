bd17ed4d8beff6f16afa6466909726c59625fe68
DENNIS <dennismwaura074@gmail.com>
2025-10-16 23:54:35 +0300

    create commandline runner

--- src/main/java/com/dennis/vehicleRentalManagement/VehicleRentalManagementApplication.java
+++ src/main/java/com/dennis/vehicleRentalManagement/VehicleRentalManagementApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
@@ -43,9 +43,5 @@ public class VehicleRentalManagementApplication {
         };
     }
 
-    // Password encoder bean
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
