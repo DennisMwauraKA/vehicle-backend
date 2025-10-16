93cba714f6dfbc2d92817353d322476d0a8a2a65
DENNIS <dennismwaura074@gmail.com>
2025-10-16 22:02:18 +0300

    updated admin post contruct

---
+++ src/main/java/com/dennis/vehicleRentalManagement/configurations/Initializer.java
@@ -0,0 +1,50 @@
+package com.dennis.vehicleRentalManagement.configurations;
+import com.dennis.vehicleRentalManagement.authorization.Role;
+import com.dennis.vehicleRentalManagement.entity.User;
+import com.dennis.vehicleRentalManagement.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Initializer {
+
+    private final UserRepository userRepository;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @Value("${admin.email}")
+    private String email;
+
+    @Value("${admin.firstname}")
+    private String adminFirstName;
+
+    @Value("${admin.lastname}")
+    private String adminLastName;
+
+    @PostConstruct
+    public void init() {
+        try {
+            // Check if admin exists
+            User adminAccount = userRepository.findByRole(Role.ADMIN);
+            if (adminAccount == null) {
+                User user = new User();
+                user.setEmail(email);
+                user.setFirstName(adminFirstName);
+                user.setLastName(adminLastName);
+                user.setRole(Role.ADMIN);
+                user.setPassword(new BCryptPasswordEncoder().encode(adminPassword));
+                userRepository.save(user);
+                System.out.println(" Admin account created successfully!");
+            } else {
+                System.out.println(" Admin account already exists, skipping creation.");
+            }
+        } catch (Exception e) {
+            System.err.println(" Error creating admin account: " + e.getMessage());
+        }
+    }
+}
